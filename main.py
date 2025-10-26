dc5e3997cf230eadab06b55d3cd9465f0b7495fc
NoahjmHolt <noahjmholt@gmail.com>
2025-10-26 13:22:03 -0600

    formatting issues
    formatting issues for boot.dev turn in.
    
    fixed and submission excepted

--- main.py
+++ main.py
@@ -12,11 +12,11 @@ client = genai.Client(api_key=api_key)
 def main():
     response = client.models.generate_content(
         model='gemini-2.0-flash-001',
-        contents='"Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."'
+        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
     )
     print(response.text)
-    print(f"Prompt Tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response Tokens: {response.usage_metadata.candidates_token_count}")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 
 if __name__ == "__main__":
