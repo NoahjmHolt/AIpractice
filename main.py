d3ac54249387e6290019a91fd0276460c02b32c5
NoahjmHolt <noahjmholt@gmail.com>
2025-10-26 13:55:47 -0600

    conversations later
    setting up to make conversations with the agent later instead of just single questions

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import sys
 
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -17,9 +18,15 @@ def main(*args, **kwargs):
         print("Error: No prompt provided")
         sys.exit(1)
 
+    user_prompt = sys.argv[1]
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+
     response = client.models.generate_content(
         model='gemini-2.0-flash-001',
-        contents=sys.argv[1]
+        contents=messages
     )
 
     print(response.text)
