f859e925f577697f1798a2987e5f2bf7b5c5798f
NoahjmHolt <noahjmholt@gmail.com>
2025-10-26 13:35:40 -0600

    now accepting arg as prompt
    now able to call the func and pass it a prompt from the terminal

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 
 import os
+import sys
+
 from dotenv import load_dotenv
 from google import genai
 
@@ -9,11 +11,17 @@ api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
 
-def main():
+def main(*args, **kwargs):
+
+    if not sys.argv[1]:
+        print("Error: No prompt provided")
+        sys.exit(1)
+
     response = client.models.generate_content(
         model='gemini-2.0-flash-001',
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+        contents=sys.argv[1]
     )
+
     print(response.text)
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
